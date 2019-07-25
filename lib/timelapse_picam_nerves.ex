166edd6222506a3f199aa67aa0032b5ade182d6c
Julian Rubisch <julian@julianrubisch.at>
2019-07-25 10:38:55 +0200

    Initial commit

---
+++ lib/timelapse_picam_nerves.ex
@@ -0,0 +1,18 @@
+defmodule TimelapsePicamNerves do
+  @moduledoc """
+  Documentation for TimelapsePicamNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TimelapsePicamNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/timelapse_picam_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule TimelapsePicamNervesTest do
+  use ExUnit.Case
+  doctest TimelapsePicamNerves
+
+  test "greets the world" do
+    assert TimelapsePicamNerves.hello() == :world
+  end
+end
